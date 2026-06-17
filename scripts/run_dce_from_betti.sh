cebe5942361c9f0bc32b36726cb9117617f86799
Dhruv Upreti <dhruvupreti05@gmail.com>
2026-06-17 14:07:06 -0400

    betti1 and pmax! 😼😼😼

--- scripts/run_dce_from_betti.sh
+++ scripts/run_dce_from_betti.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=tda_multi_species
 #SBATCH --account=physics       
 #SBATCH --partition=cpu         
-#SBATCH --qos=normal          
+#SBATCH --qos=standby         # changed temorarly since `squeue -A physics` is busy
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=50     # Matches your N_runs=50 perfectly
-#SBATCH --mem=200G             # Ensure ~4G per process, 200G is great
-#SBATCH --time=2-00:00:00      # Increased to 48 hours to avoid mid-run timeouts
+#SBATCH --cpus-per-task=50    
+#SBATCH --mem=200G            
+#SBATCH --time=04:00:00      # max allowed for standby
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
